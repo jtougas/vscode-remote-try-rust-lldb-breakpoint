aaef2686dcf7710c6716657f6ca771216609fe47
Jonathan Tougas <jtougas@gmail.com>
2021-04-28 17:40:20 +0000

    repro for https://github.com/tokio-rs/tracing/issues/1363

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+// uncomment this line = no more breakpoints :(
+// use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
+
+
 /*--------------------------------------------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
 
-fn main() {
+ fn main() {
     let name = "VS Code Remote - Containers";
     println!("Hello, {}!", name);
 }
